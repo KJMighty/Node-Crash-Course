1e01a70e44326727f8303065d08c58b8e1dc2cd2
KJMighty <kaimighty@hotmail.co.uk>
2021-04-21 22:29:31 +0100

    update code and add files

--- index.js
+++ index.js
@@ -1,9 +1,82 @@
-const Logger = require('./logger');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const { ENOENT } = require('constants');
+const server = http.createServer((req, res) => {
+//   if(req.url === '/') {
+//     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+//         if(err) throw err;
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.end(content);
+//     })
 
-const logger = new Logger();
+//   }
 
-logger.on('message', data => {
-    console.log('Called listener in', data);
-});
+//   if(req.url === '/api/users') {
+//       const users = [
+//           {
+//               name: 'Bob Smith',
+//               age: 40,
+//           },
+//           {
+//               name: 'John Doe',
+//               age: 35
+//           }
+//       ];
+//       res.writeHead(200, { 'Content-Type': 'application/json' });
+//       res.end(JSON.stringify(users));
+//   }
 
-logger.log('Hello World!');
+   //Build file path
+   const filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+   //Extension file
+   const extName = path.extname(filePath);
+
+   //Initial content type
+   let contentType = 'text/html';
+
+   //Check ext and set content type
+   switch(extName) {
+       case '.js':
+           contentType = 'text/javascript';
+           break;
+       case '.css':
+           contentType = 'text/css';
+           break;
+       case '.json':
+           contentType = 'application/json';
+           break;
+       case '.png':
+           contentType = 'image/png';
+           break;
+       case '.jpg':
+           contentType = 'image.jpg';
+           break;
+   }
+
+   //Read file path
+   fs.readFile(filePath, (err, content) => {
+       if(err) {
+           if(err.code === 'ENOENT') {
+               //PAGE NOT FOUND
+               fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content, 'utf8');
+               })
+           } else {
+               //Some server error
+               res.writeHead(500);
+               res.end(`Server error ${err.code}`)
+           }
+       } else {
+           //Successful response
+           res.writeHead(200, { 'Content-Type': contentType });
+           res.end(content, 'utf8');
+       }
+   })
+})
+
+const PORT = process.env.port || 5000;
+
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
---
+++ reference/http_demo.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+//Create server object
+http.createServer((request, response) => {
+    response.write('Hello World!');
+    response.end()
+}).listen(5000, () => {console.log('Server running....')});
